f6c8a7d2599e0fe9424cfa24af968e40541eff48
Remix007 <forvard16back@gmail.com>
2023-02-11 00:18:26 +0200

    phonebook jsx

---
+++ src/components/PhoneBookList/PhoneBookList.jsx
@@ -0,0 +1,21 @@
+import s from './PhoneBookList.module.css';
+import PropTypes from 'prop-types';
+
+const PhoneBookList = ({ phoneList, onDeletePhoneListItem }) => {
+  return (
+    <ul className={s.list}>
+      {phoneList.map(({ id, name, number }) => (
+        <li key={id} className={s.phoneBookListItem}>
+          <p className={s.nameText}>{name}</p>
+          <p className={s.phoneNumber}>{number}</p>
+          <button
+            className={s.buttonDelete}
+            onClick={() => onDeletePhoneListItem(id)}
+          >
+            delete
+          </button>
+        </li>
+      ))}
+    </ul>
+  );
+};
