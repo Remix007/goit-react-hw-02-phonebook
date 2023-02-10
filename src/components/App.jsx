68aa7eb63f8fbcff8f800e3b7af1a98362c38a52
Remix007 <forvard16back@gmail.com>
2023-02-10 12:57:59 +0200

    app add

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+class App extends Component {
+  state = {
+    phoneList: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    filter: '',
+  };
+
+  formSubmitHendler = data => {
+    const {phoneList}= this.state;
+    const phoneBookListItem = {
+      id: nanoid(),
+      name: data.name,
+      number: data.number,
+    };
+    if (phoneList.find(item => item.name === phoneBookListItem.name)) {
+      return alert(`${phoneBookListItem.name}  is olrady in contacts`);
+    }
+    this.setState(prevState =>({
+      phoneList: [phoneBookListItem, ...prevState.phoneList],
+    }))
+  }
